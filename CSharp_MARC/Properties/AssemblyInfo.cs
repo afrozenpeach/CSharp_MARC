acfa802608433b33a3e4c5af4ad667ffc1ca6bb6
frozen-solid <frozen-solid@6e2caeb4-2541-4f6d-96d9-6ac2c13aabe1>
2012-01-11 16:50:08 +0000

    Updated copyright information for 2012

--- CSharp_MARC/Properties/AssemblyInfo.cs
+++ CSharp_MARC/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bound To Stay Bound Books")]
 [assembly: AssemblyProduct("CSharp_MARC")]
-[assembly: AssemblyCopyright("Copyright © Matt Schraeder and Bound To Stay Bound Books 2009-2011")]
+[assembly: AssemblyCopyright("Copyright © Matt Schraeder and Bound To Stay Bound Books 2009-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -58,5 +58,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.0.1.2")]
-[assembly: AssemblyFileVersion("0.0.1.2")]
+[assembly: AssemblyVersion("0.0.1.3")]
+[assembly: AssemblyFileVersion("0.0.1.3")]
