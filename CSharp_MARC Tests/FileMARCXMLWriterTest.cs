d5ff96f18a274c42cc1f493a3c9f4d9d64946683
frozen-solid <frozen-solid@6e2caeb4-2541-4f6d-96d9-6ac2c13aabe1>
2014-02-01 06:00:51 +0000

    It helps to actually add the new files, rather than just updating the old ones

---
+++ CSharp_MARC Tests/FileMARCXMLWriterTest.cs
@@ -0,0 +1,120 @@
+﻿using MARC;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSharp_MARC_Tests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for FileMARCXMLWriterTest and is intended
+    ///to contain all FileMARCXMLWriterTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class FileMARCXMLWriterTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Write
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("Test Records\\onerecord.xml")]
+        public void WriteTest()
+        {
+            string filename = "onerecord.xml";
+            string testFilename = "onerecord_test.xml";
+            string source = File.ReadAllText(filename);
+            FileMARCXML targetXML = new FileMARCXML(source);
+            Record record = targetXML[0];
+
+            using (FileMARCXMLWriter target = new FileMARCXMLWriter(testFilename))
+            {
+                target.Write(record);
+            }
+
+            string expected = source;
+            string actual = File.ReadAllText(testFilename);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for Write
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("Test Records\\music.xml")]
+        public void WriteTest1()
+        {
+            string filename = "music.xml";
+            string testFilename = "music_test.xml";
+            string source = File.ReadAllText(filename);
+            FileMARCXML targetXML = new FileMARCXML(source);
+            List<Record> records = new List<Record>();
+
+            foreach (Record record in targetXML)
+            {
+                records.Add(record);
+            }
+
+            using (FileMARCXMLWriter target = new FileMARCXMLWriter(testFilename))
+            {
+                target.Write(records);
+            }
+
+            string expected = source;
+            string actual = File.ReadAllText(testFilename);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CSharp_MARC/FileMARCXMLWriter.cs
@@ -0,0 +1,99 @@
+﻿/**
+ * Parser for MARC records
+ *
+ * This project is based on the File_MARC package
+ * (http://pear.php.net/package/File_MARC) by Dan Scott , which was based on PHP
+ * MARC package, originally called "php-marc", that is part of the Emilda
+ * Project (http://www.emilda.org). Both projects were released under the LGPL
+ * which allowed me to port the project to C# for use with the .NET Framework.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Matt Schraeder <mschraeder@csharpmarc.net> <mschraeder@btsb.com>
+ * @copyright 2009-2014 Matt Schraeder and Bound to Stay Bound Books <http://www.btsb.com>
+ * @license   http://www.gnu.org/copyleft/lesser.html  LGPL License 3
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Linq;
+
+namespace MARC
+{
+    public class FileMARCXMLWriter : IDisposable
+    {
+        //Member Variables and Properties
+        #region Member Variables and Properties
+
+        private string filename = null;
+        private StreamWriter writer = null;
+
+        #endregion
+
+		//Constructors
+		#region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileMARCWriter"/> class.
+        /// </summary>
+        /// <param name="filename">The filename.</param>
+        /// <param name="mode">The FileMode.</param>
+        public FileMARCXMLWriter(string filename)
+        {
+            this.filename = filename;
+
+			writer = new StreamWriter(filename, false, Encoding.UTF8);
+        }
+
+		#endregion
+
+        /// <summary>
+        /// Writes the specified record.
+        /// </summary>
+        /// <param name="record">The record.</param>
+        public void Write(Record record)
+        {
+            XDocument xml = record.ToXMLDocument();
+
+            xml.Save(writer);
+        }
+
+        /// <summary>
+        /// Writes the specified records.
+        /// </summary>
+        /// <param name="records">The records.</param>
+        public void Write(List<Record> records)
+        {
+            XDocument xml = new XDocument(new XDeclaration("1.0", "utf-8", null));
+            XElement collection = new XElement(FileMARCXML.Namespace + "collection");
+            xml.Add(collection);
+
+            foreach (Record record in records)
+                collection.Add(record.ToXML());
+
+            xml.Save(writer);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            writer.Dispose();
+        }
+    }
+}
